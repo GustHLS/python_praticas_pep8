e74aeba8e54f321a69d69a4081565f0c37e9ad0a
gusthls <gustavohls_work@hotmail.com>
2025-03-31 11:27:30 -0300

    adição dos testes para as rotas

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,3 +7,34 @@ def test_home():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"mensagem": "Bem-vindo à API de Recomendação de Produtos"}
+
+def test_criar_produtos():
+    response = client.post("/produtos/", json={
+        "nome": "Produtos A", 
+        "categoria": "Categoria 1", 
+        "tags": ["tag1", "tag2"]
+        },
+    )
+    assert response.status_code == 200
+    assert response.json()["nome"] == "Produtos A"
+
+def test_listar_produtos():
+    response = client.get("/produtos/")
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+
+def test_criar_usuarios():
+    response = client.post("/usuarios", params={
+        "nome": "Usuário 1"
+        },
+    )
+    assert response.status_code == 200
+
+    usuario_data = response.json()
+    assert usuario_data["id"] == 1
+    assert usuario_data["nome"] == "Usuário 1"
+
+def test_listar_usuarios():
+    response = client.get("/usuarios/")
+    assert response.status_code == 200
+    assert len(response.json()) == 1
