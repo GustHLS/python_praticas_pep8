e493e9b8692daaa39ce0a8ff351d808168c3e852
gusthls <gustavohls_work@hotmail.com>
2025-03-31 10:18:19 -0300

    organizando models e routers

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from typing import Dict
+from fastapi import FastAPI # type: ignore
+from app.routers import routers_produtos, routers_usuarios
+
+MENSAGEM_HOME: str = "Bem-vindo à API de Recomendação de Produtos"
+
+
+# Criando o App
+app = FastAPI()
+
+app.include_router(routers_usuarios.router)
+app.include_router(routers_produtos.router)
+
+# Iniciando o servidor
+@app.get("/")
+def home() -> Dict[str, str]:
+    global ConstanteMensagemHome
+    return {"mensagem": ConstanteMensagemHome}
+
---
+++ app/models/models_usuarios.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel # type: ignore
+
+# Modelo base para um usuário
+class Usuario(BaseModel):
+    id: int
+    nome: str
